f030976161b7f22a4ca6502ea791307ed5fc64f5
EnzoValim07 <enzo.pereira@sptech.school>
2026-05-09 19:05:10 -0300

    mudando a logica do quiz

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -14,7 +14,7 @@ function autenticar(req, res) {
             .then(
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
@@ -37,18 +37,15 @@ function autenticar(req, res) {
                 }
             );
     }
-
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
     var conhece = req.body.conheceServer;
 
-    // Faça as validações dos valores
-     if (nome == undefined) {
+    if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
@@ -58,11 +55,14 @@ function cadastrar(req, res) {
         res.status(400).send("A resposta está undefined!");
     } else {
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
         usuarioModel.cadastrar(nome, email, senha, conhece)
             .then(
                 function (resultado) {
-                    res.json(resultado);
+                    res.status(200).json({
+                        id: resultado.insertId,
+                        nome: nome,
+                        email: email
+                    });
                 }
             ).catch(
                 function (erro) {
